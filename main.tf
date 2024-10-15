01540a63a776943f33d3a84492f599caf6aa70a1
Kazuhiro Umemura <supernovatypebiz@gmail.com>
2024-10-15 17:16:14 +0900

    modify default route for private subnet

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "aws_route_table" "private_route_table2" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    network_interface_id = module.cc_vm.service_eni_1[0]
+    network_interface_id = module.cc_vm.service_eni_1[1]
   }
   tags = {
     Name = "${var.aws_vpc_name}-private_route_table2"
